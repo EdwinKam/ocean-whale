4b02e8b4541cdbbdf5dcdceb46023e75991fb27e
ekam <ekam@netflix.com>
2024-11-14 23:52:16 -0800

    able to verify auth now

---
+++ src/main/java/com/ocean/whale/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.ocean.whale.controller;
+
+import com.google.firebase.auth.FirebaseAuthException;
+import com.ocean.whale.model.VerifyAuthResponse;
+import com.ocean.whale.service.auth.GoogleAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final GoogleAuthService googleAuthService;
+
+    @Autowired
+    public AuthController(GoogleAuthService googleAuthService) {
+        this.googleAuthService = googleAuthService;
+    }
+
+    @GetMapping("/verify")
+    public VerifyAuthResponse verifyAuth(@RequestParam String accessToken) {
+        // http://localhost:8080/auth/verify?accessToken=eyJxxxx
+        VerifyAuthResponse verifyAuthResponse = new VerifyAuthResponse();
+        try {
+            Optional<String> uid = googleAuthService.getUid(accessToken);
+            verifyAuthResponse.setValidToken(uid.isPresent() && !uid.get().isBlank());
+        } catch (FirebaseAuthException e) {
+            verifyAuthResponse.setErrorCode(e.getAuthErrorCode().name());
+        } catch (Exception e) {
+            //
+        }
+        return verifyAuthResponse;
+    }
+}
