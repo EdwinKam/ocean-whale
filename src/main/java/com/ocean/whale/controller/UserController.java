f261c53e653d2ce7e9a2cd993e6af5d532f511b5
ekam <ekam@netflix.com>
2024-11-16 22:04:03 -0800

    added swagger for testing

--- src/main/java/com/ocean/whale/controller/UserController.java
+++ src/main/java/com/ocean/whale/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ocean.whale.controller;
 import com.ocean.whale.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,4 @@ public class UserController {
     this.userService = userService;
   }
 
-  // TODO change to post mapping, add param for accessToken
-  @GetMapping("/create")
-  public String createUser() throws Exception {
-    userService.createUser("fsf", "sgs");
-    return "haha";
-  }
 }
