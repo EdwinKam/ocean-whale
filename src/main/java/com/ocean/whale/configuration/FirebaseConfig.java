4eed997b3928e31fb4c049d12ab09e4f633d8bdd
Anson Chung <ansoncht@gmail.com>
2024-11-16 16:30:32 -0800

    [CHORE] Replace deprecated function

--- src/main/java/com/ocean/whale/configuration/FirebaseConfig.java
+++ src/main/java/com/ocean/whale/configuration/FirebaseConfig.java
@@ -1,15 +1,16 @@
 package com.ocean.whale.configuration;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
-import com.google.cloud.firestore.Firestore;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 @Configuration
 public class FirebaseConfig {
@@ -17,9 +18,9 @@ public class FirebaseConfig {
     @Bean
     public Firestore firestore() throws IOException {
         FileInputStream serviceAccount = new FileInputStream("ocean-app-a4c89-firebase-adminsdk-urlnl-05c730aa42.json");
-        FirebaseOptions options = new FirebaseOptions.Builder()
+        FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                .setDatabaseUrl("your-firestore-database-url")
+                // .setDatabaseUrl("your-firestore-database-url")
                 .build();
         FirebaseApp.initializeApp(options);
 
