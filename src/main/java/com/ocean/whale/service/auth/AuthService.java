f2fa8c5d9712b98b35ffb9d9fc381733f850ca50
Anson Chung <ansoncht@gmail.com>
2024-11-21 19:44:23 -0800

    [CHORE] Consolidate Auth to support email / password

--- src/main/java/com/ocean/whale/service/auth/AuthService.java
+++ src/main/java/com/ocean/whale/service/auth/AuthService.java
@@ -2,7 +2,28 @@ package com.ocean.whale.service.auth;
 
 import java.util.Optional;
 
-public interface AuthService {
-    Optional<String> getUid(String accessToken) throws Exception;
-    String getUsername(String uid) throws Exception;
+import org.springframework.stereotype.Service;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.auth.UserRecord;
+
+@Service
+public class AuthService {
+    public Optional<String> getUid(String accessToken) throws Exception {
+        FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(accessToken);
+        return Optional.ofNullable(decodedToken.getUid());
+    }
+
+    public Optional<String> getUsername(String uid) throws Exception {
+        UserRecord userRecord = FirebaseAuth.getInstance().getUser(uid);
+
+        return Optional.ofNullable(userRecord.getDisplayName());
+    }
+
+    public Optional<String> getEmail(String uid) throws Exception {
+        UserRecord userRecord = FirebaseAuth.getInstance().getUser(uid);
+
+        return Optional.ofNullable(userRecord.getEmail());
+    }
 }
